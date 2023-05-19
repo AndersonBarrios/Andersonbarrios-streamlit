aef5877a0fab5b90dccb5625fff6c899124a6b2d
AndersonBarrios <134021939+AndersonBarrios@users.noreply.github.com>
2023-05-18 18:35:06 -0600

    Delete AnderBarrios.py

--- AnderBarrios.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-# using pandas read_csv function to load the dataset
-df = pd.read_csv("RTADataset.csv")
-# target variable classes counts and bar plot
-import matplotlib.pyplot as plt
-print(df['Accident_severity'].value_counts())
-print(df['Accident_severity'].value_counts().plot(kind='bar'))
